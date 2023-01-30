8dc59433076d7790996fd22ae6c22d125b01bea9
aguilar <aguilarmatt10@gmail.com>
2023-01-30 18:15:07 -0500

    camp

--- Intermediate Lab Work/AGUILAR_intermediate_HW1.py
+++ Intermediate Lab Work/AGUILAR_intermediate_HW1.py
@@ -1,9 +1,10 @@
+#%%
 import pandas as pd
 import matplotlib.pyplot as plt
 
 temp_vs_time = pd.read_csv('../temp_vs_time.csv', index_col=0, )
 temp_vs_time.head()
-temp_vs_time.plot(title = 'Matthew Aguilar')
+temp_vs_time.plot(title='Matthew Aguilar')
 plt.savefig('Matthew_Aguilar.pdf')
 plt.show()
 #%%
---
+++ Intermediate Lab Work/Lab 1/actual Lab1 Work.py
@@ -0,0 +1,9 @@
+# The resistance of the resistor is 1000 ohms.
+# Max power supplied to circuit 0.125 W
+import math
+voltage_Max = math.sqrt((0.125*1000))
+print(voltage_Max)
+# Resolution of Ammeter is 0.01 mA with accuracy of 1.5% + 2 counts.
+
+
+#%%
